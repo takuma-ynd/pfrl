8d3f2bfad042b02f12efcf6262f1a700ee4a6dab
Takuma Yoneda <takuma-ynd@users.noreply.github.com>
2020-08-13 23:07:16 +0900

    change default use_tensorboard value to True

--- pfrl/experiments/evaluator.py
+++ pfrl/experiments/evaluator.py
@@ -393,7 +393,7 @@ class Evaluator(object):
         step_offset=0,
         save_best_so_far_agent=True,
         logger=None,
-        use_tensorboard=False,
+        use_tensorboard=True,
     ):
         assert (n_steps is None) != (n_episodes is None), (
             "One of n_steps or n_episodes must be None. "
